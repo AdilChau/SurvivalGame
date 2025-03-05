17a7976d5df22752597ae522f6c291243e10b093
Adil Chaudhry <adilchaudhry2408@gmail.com>
2025-03-05 16:05:35 +0000

    Improved Tilemap and Added different terrain

---
+++ Assets/Tiles/CustomeTile.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Tile", menuName = "Tiles/Custom Tile")]
+public class CustomTile : Tile
+{
+    // Can add custome behaviours here if need be
+}
